6dfadec21cecb24b51001a578b9a1044a50c44b3
Yyin <yyiiiiiin@gmail.com>
2026-02-15 23:10:53 +0800

    Add files via upload

---
+++ Otogibanashi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Algebra's like sheet music.\n");
+    printf("The important thing isn't \"can you read music?\".\n");
+    printf("It's \"can you hear it?\".\n");
+    printf("CAN YOU HEAR THE MUSIC?");  
+
+    return 0 ;
+}
